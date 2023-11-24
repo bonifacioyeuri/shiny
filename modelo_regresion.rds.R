cc9601f975d93f22f7c4083554f848ae293e23bb
bonifacioyeuri <151546139+bonifacioyeuri@users.noreply.github.com>
2023-11-24 18:29:45 -0500

    Add files via upload

---
+++ modelo_regresion.rds.R
@@ -0,0 +1,17 @@
+
+library(caret)
+library(ggplot2)
+
+
+url2<- "https://raw.githubusercontent.com/bonifacioyeuri/shiny/main/ciclocombinado.csv"
+ccombinado <- read.csv(url2)
+
+predictores <- ccombinado[,c("ccombinado$Año")]
+objetivo <- ccombinado$TE
+
+train_control <- trainControl(method = "cv", number=10)
+
+modelo <- train(predictores, objetivo, method="lm", trControl=train_control)
+
+saveRDS(modelo, file = "/modelo_regresion.rds")
+
